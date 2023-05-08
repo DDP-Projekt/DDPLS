9038225d051ef6ad4dfc50f68311bcde1a4e934f
Hendrik Ziegler <hendrikziegler@outlook.de>
2023-05-08 20:16:55 +0200

    added caller information to the logger for more debug info in the future

--- log/log.go
+++ log/log.go
@@ -1,17 +1,31 @@
 package log
 
-import "github.com/tliron/kutil/logging"
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/tliron/kutil/logging"
+)
+
+func fmtWithCaller(format string) string {
+	pc, _, no, ok := runtime.Caller(2)
+	details := runtime.FuncForPC(pc)
+	if ok && details != nil {
+		return fmt.Sprintf("[%s: %d] %s", details.Name(), no, format)
+	}
+	return fmt.Sprintf("[no info] %s", format)
+}
 
 var logger = logging.GetLogger("ddp.ddpls")
 
 func Errorf(format string, args ...any) {
-	logger.Errorf(format, args...)
+	logger.Errorf(fmtWithCaller(format), args...)
 }
 
 func Warningf(format string, args ...any) {
-	logger.Warningf(format, args...)
+	logger.Warningf(fmtWithCaller(format), args...)
 }
 
 func Infof(format string, args ...any) {
-	logger.Infof(format, args...)
+	logger.Infof(fmtWithCaller(format), args...)
 }

d4cefa4c5b8d03ed192d3609740c5fbd1af5f39b
Hendrik Ziegler <hendrikziegler@outlook.de>
2023-09-21 23:16:43 +0200

    added  cpuprofile command-line flag to capture performance profiles

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"flag"
+	"os"
+	"runtime/pprof"
+
 	"github.com/DDP-Projekt/DDPLS/ddpls"
+	"github.com/DDP-Projekt/DDPLS/log"
 	"github.com/tliron/kutil/logging"
 
 	// Must include a backend implementation. See kutil's logging/ for other options.
@@ -9,9 +14,28 @@ import (
 )
 
 func main() {
+	var cpuprofile string
+	flag.StringVar(&cpuprofile, "cpuprofile", "", "write cpu profile to file")
+	flag.Parse()
+
 	// This increases logging verbosity (optional)
 	logging.Configure(1, nil)
 
-	ddpls := ddpls.NewDDPLS()
-	ddpls.Server.RunStdio()
+	ls := ddpls.NewDDPLS()
+
+	if cpuprofile != "" {
+		f, err := os.Create(cpuprofile)
+		if err != nil {
+			log.Warningf("error creating cpuprofile file: %w", err)
+		} else {
+			defer f.Close()
+			if err := pprof.StartCPUProfile(f); err != nil {
+				log.Warningf("error starting cpuprofile: %w", err)
+			} else {
+				defer pprof.StopCPUProfile()
+			}
+		}
+	}
+
+	ls.Server.RunStdio()
 }

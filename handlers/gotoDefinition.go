f4b4a28c47a4989f23d477b16bd689950c73adc4
bafto <hendrikziegler@outlook.de>
2024-11-19 12:42:56 +0100

    added gotodefinition for types

--- handlers/gotoDefinition.go
+++ handlers/gotoDefinition.go
@@ -7,6 +7,8 @@ import (
 	"github.com/DDP-Projekt/DDPLS/helper"
 	"github.com/DDP-Projekt/DDPLS/uri"
 	"github.com/DDP-Projekt/Kompilierer/src/ast"
+	"github.com/DDP-Projekt/Kompilierer/src/ddptypes"
+	"github.com/DDP-Projekt/Kompilierer/src/token"
 	"github.com/tliron/glsp"
 	protocol "github.com/tliron/glsp/protocol_3_16"
 )
@@ -41,14 +43,73 @@ type definitionVisitor struct {
 }
 
 var (
-	_ ast.Visitor            = (*definitionVisitor)(nil)
-	_ ast.ConditionalVisitor = (*definitionVisitor)(nil)
+	_ ast.Visitor              = (*definitionVisitor)(nil)
+	_ ast.ConditionalVisitor   = (*definitionVisitor)(nil)
+	_ ast.VarDeclVisitor       = (*definitionVisitor)(nil)
+	_ ast.FuncDeclVisitor      = (*definitionVisitor)(nil)
+	_ ast.TypeDefDeclVisitor   = (*definitionVisitor)(nil)
+	_ ast.TypeAliasDeclVisitor = (*definitionVisitor)(nil)
 )
 
 func (*definitionVisitor) Visitor() {}
 
 func (def *definitionVisitor) ShouldVisit(node ast.Node) bool {
 	return helper.IsInRange(node.GetRange(), def.pos)
+}
+
+func (def *definitionVisitor) VisitVarDecl(d *ast.VarDecl) ast.VisitResult {
+	if helper.IsInRange(d.TypeRange, def.pos) {
+		def.gotoType(d.Type)
+		return ast.VisitBreak
+	}
+	return ast.VisitRecurse
+}
+
+func (def *definitionVisitor) VisitFuncDecl(d *ast.FuncDecl) ast.VisitResult {
+	if helper.IsInRange(d.ReturnTypeRange, def.pos) {
+		def.gotoType(d.ReturnType)
+		return ast.VisitBreak
+	}
+
+	for _, param := range d.Parameters {
+		if helper.IsInRange(param.TypeRange, def.pos) {
+			def.gotoType(param.Type.Type)
+			return ast.VisitBreak
+		}
+	}
+
+	return ast.VisitRecurse
+}
+
+func (def *definitionVisitor) VisitStructDecl(d *ast.StructDecl) ast.VisitResult {
+	for _, field := range d.Fields {
+		d, ok := field.(*ast.VarDecl)
+		if !ok {
+			continue
+		}
+
+		if helper.IsInRange(d.TypeRange, def.pos) {
+			def.gotoType(d.Type)
+			return ast.VisitBreak
+		}
+	}
+	return ast.VisitRecurse
+}
+
+func (def *definitionVisitor) VisitTypeAliasDecl(d *ast.TypeAliasDecl) ast.VisitResult {
+	if helper.IsInRange(d.UnderlyingRange, def.pos) {
+		def.gotoType(d.Underlying)
+		return ast.VisitBreak
+	}
+	return ast.VisitRecurse
+}
+
+func (def *definitionVisitor) VisitTypeDefDecl(d *ast.TypeDefDecl) ast.VisitResult {
+	if helper.IsInRange(d.UnderlyingRange, def.pos) {
+		def.gotoType(d.Underlying)
+		return ast.VisitBreak
+	}
+	return ast.VisitRecurse
 }
 
 func (def *definitionVisitor) VisitIdent(e *ast.Ident) ast.VisitResult {
@@ -92,4 +153,37 @@ func (def *definitionVisitor) getUri(decl ast.Declaration) string {
 		uri_ = mod.Uri
 	}
 	return string(uri_)
+}
+
+func (def *definitionVisitor) gotoType(typ ddptypes.Type) {
+	if typ == nil {
+		return
+	}
+
+	if lt, ok := typ.(*ddptypes.ListType); ok {
+		def.gotoType(lt.Underlying)
+		return
+	}
+
+	decl, exists, _ := def.docMod.Ast.Symbols.LookupDecl(typ.String())
+	if !exists {
+		return
+	}
+
+	uri := def.getUri(decl)
+	Range := token.Range{}
+
+	switch decl := decl.(type) {
+	case *ast.StructDecl:
+		Range = decl.NameTok.Range
+	case *ast.TypeDefDecl:
+		Range = decl.NameTok.Range
+	case *ast.TypeAliasDecl:
+		Range = decl.NameTok.Range
+	}
+
+	def.location = &protocol.Location{
+		URI:   uri,
+		Range: helper.ToProtocolRange(Range),
+	}
 }
